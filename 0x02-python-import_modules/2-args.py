2d62b17665cc5e9b4b11c0cc12446aa726eaa7d9
Liliana327 <894@holbertonschool.com>
2019-09-12 22:55:45 +0000

    list argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,11 +3,11 @@
 if __name__ == "__main__":
     from sys import argv
     if len(argv) == 1:
-        s = ''argument.'''
+        s = '''argument.'''
     elif len(argv) == 2:
         s = '''argument:'''
     else:
-        s = '''argument:''
+        s = '''argument:'''
     print("{} {}".format(len(argv) - 1, s))
     number = 1
     for m in argv[1:]:
