e55bf70a728fbab372178a55d9210732640942da
Liliana327 <894@holbertonschool.com>
2019-09-12 22:42:09 +0000

    list argv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from sys import argv
+    if len(argv) == 1:
+        s = '''arguments.'''
+    elif len (argv) == 2:
+        s = '''arguments:'''
+    else:
+        s = '''arguments:'''
+    print("{} {}".format(len(argv) - 1, s))
+    number = 1
+    for m in argv[1:]:
+        print ("{}: {}".format(number, m))
+        number += 1
