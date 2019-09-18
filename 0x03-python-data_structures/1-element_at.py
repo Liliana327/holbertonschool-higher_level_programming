6ad85d9d9757350600f935347e7661e7745cb4f0
Liliana327 <51680172+Liliana327@users.noreply.github.com>
2019-09-17 23:37:35 -0500

    add task number 1

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+        if idx > len(my_list) - 1:
+            return None
+        elif idx < 0:
+            return None
+        else:
+            for m in range(len(my_list)):
+                    if m == idx:
+                        return my_list[m]
+                    else:
+                        continue
