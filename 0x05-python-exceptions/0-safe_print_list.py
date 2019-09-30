ce7e10f19c2a29512db0dbc38eafe3b42a692ce3
Liliana327 <894@holbertonschool.com>
2019-09-30 21:35:45 +0000

    add task number zero and readme

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,8 +5,8 @@ def safe_print_list(my_list=[], x=0):
         while m < x:
             print('''{}'''.format(my_list[m]), end='')
             m += 1
-        print ('')
+        print('')
         return m
     except IndexError:
-        print ('')
+        print('')
         return m
