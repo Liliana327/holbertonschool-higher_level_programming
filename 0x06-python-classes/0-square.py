0e958d87a565e6b93a547e1a5a2434bdefcdf047
Liliana327 <894@holbertonschool.com>
2019-10-02 01:16:41 +0000

    Write an empty class Square, and Readme

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" This is a function that creates a Square class
+
+Create an empty class square to be used by the test file 0-main.py
+
+Example:
+    ./0-main.py
+
+"""
+
+
+class Square:
+    """Create an empty class called Square
+
+    Attributes:
+        None
+
+    """
+    pass
