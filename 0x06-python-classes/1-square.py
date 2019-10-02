ccb2d11852cca68c4f14cd55e1a1437d100abdf1
Liliana327 <894@holbertonschool.com>
2019-10-02 02:03:17 +0000

    defines a square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" This function defines a Square
+
+    It will be used by the test file based on 0-square.py
+
+    Example:
+        ./0-main.py
+"""
+
+class Square:
+    """ Creates a class Square which has a size
+    """
+    def __init__ (self, size):
+        """ Initializes instance attributes
+
+            (int) is the size of the square.
+
+        """
+        self.__size = size
