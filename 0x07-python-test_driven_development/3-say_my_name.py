014c84b2ccdfccc5555f5a0c2232b3db693f4e93
Liliana327 <894@holbertonschool.com>
2019-10-05 18:47:59 +0000

    add

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -14,4 +14,4 @@ def say_my_name(first_name, last_name=""):
     if not isinstance(last_name, str):
         raise TypeError(Error_2)
 
-    print(first_name, last_name)
+    print("My name is {:s} {:s}".format(first_name, last_name))
