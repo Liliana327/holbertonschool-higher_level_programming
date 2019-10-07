5d50463f325fcf2207c757200697fa6a438fe894
Liliana327 <894@holbertonschool.com>
2019-10-07 23:33:09 +0000

    add task number 4

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+function that prints a text with 2 new lines 
+after each of these characters: ., ? and :
+"""
+
+
+def text_indentation(text):
+
+    if not isinstance(text, str):
+        raise TypeError('text must be a string')
+
+    for M in range(len(text)):
+        if text[M] == ' ' and text[M + 1] is ' ':
+            continue
+        if text[M] is ' ' and text[M - 1] is '.':
+            continue
+        if text[M] is ' ' and text[M - 1] is '?':
+            continue
+        if text[M] is ' ' and text[M - 1] is ':':
+            continue
+        if text[M] is ' ' and text[M - 1] is ' ':
+            continue
+        print(text[M], end='')
+        if text[M] is '.' or text[M] is '?' or text[M] is ':':
+            print()
+            print()
