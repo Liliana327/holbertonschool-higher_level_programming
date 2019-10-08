de59cdfb16da146f8afa017f4f6458fc773784a6
Liliana327 <894@holbertonschool.com>
2019-10-08 04:10:47 +0000

    class Rectangle that defines a rectangle by: (based on 4-rectangle.py)

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+'''
+class Rectangle that defines a by: (based on 2-rectangle.py)
+'''
+
+
+class Rectangle:
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+
+        return self.width * self.height
+
+    def perimeter(self):
+
+        if self.width == 0 or self.height == 0:
+            return 0
+        return (self.width + self.height) * 2
+
+    def __str__(self):
+
+        string = ""
+        if self.width == 0 or self.height == 0:
+            return string
+        for a in range(self.height - 1):
+            string += '#' * self.width + '\n'
+        string += '#' * self.width
+        return string
+
+    def __repr__(self):
+        esto0 = self.__class__.__name__
+        esto1 = "{}({}, {})".format(esto0, self.width, self.height)
+        string = esto1
+        return string
+    
+    def __del__(self):
+        print("Bye rectangle...")
