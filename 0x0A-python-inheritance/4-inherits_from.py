ef85f3b97e78f05446a364be362a6428166cf792
Liliana327 <894@holbertonschool.com>
2019-10-16 22:04:57 +0000

    modified

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -2,7 +2,8 @@
 """
 """
 
+
 def inherits_from(obj, a_class):
     """
     """
-    return (issubclass(type(obj), a_class) and type (obj) != a_class)
+    return(issubclass(type(obj), a_class) and type(obj) != a_class)
