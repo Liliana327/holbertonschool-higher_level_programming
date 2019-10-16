0dee83a9e9925e69b693357a69e8c1127575cbba
Liliana327 <894@holbertonschool.com>
2019-10-16 21:40:22 +0000

    modified

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """
 """
+
+
 def lookup(obj):
     """
     Return the attributes
--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,8 +4,5 @@
 
 class MyList(list):
 
-    def __init__(self):
-        list.__init__(self)
-
     def print_sorted(self):
         print(sorted(self))
