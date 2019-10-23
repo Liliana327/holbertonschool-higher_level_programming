a929382e97fd84f030f99bcfafd06d9b42b8cd1e
Liliana327 <894@holbertonschool.com>
2019-10-23 02:34:44 +0000

    add tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,28 +1,112 @@
 #!/usr/bin/python3
-
+'''
+Unit tests for Base class
+'''
 import unittest
-import json
-import pep8
-import os
 from models.base import Base
-from models.rectangle import Rectangle
 from models.square import Square
+import json
 
 
-class TestBase(unittest.TestCase):
+class test_base(unittest.TestCase):
+    '''
+    Tests for Base
+    '''
+    def test_id1(self):
+        '''
+        Testing no id sent
+        '''
+        i1 = Base()
+        self.assertEqual(1, i1.id)
 
-    def test_id(self):
+    def test_id2(self):
+        '''
+        Testing a valid id
+        '''
+        i1 = Base(33)
+        self.assertEqual(33, i1.id)
 
-        L1 = Base()
-        self.assertEqual(L1.id, 1)
-        L2 = Base()
-        self.assertEqual(L2.id, 2)
-        L3 = Base(33)
-        self.assertEqual(L3.id, 33)
-        L4 = Base()
-        self.assertEqual(L4.id, 3)
+    def test_id3(self):
+        '''
+        Testing a negative id
+        '''
+        i1 = Base(-33)
+        self.assertEqual(-33, i1.id)
 
-    def test_instance(self):
+    def test_id4(self):
+        '''
+        Testing an id of 0
+        '''
+        i1 = Base(0)
+        self.assertEqual(0, i1.id)
 
-        base1 = Base()
-        self.assertIsInstance(base1, object)
+    def test_id5(self):
+        '''
+        Testing a non-int value for id
+        '''
+        i1 = Base("hi")
+        self.assertEqual("hi", i1.id)
+
+    def test_id5(self):
+        '''
+        Testing a negative id
+        '''
+        i1 = Base(-33)
+        self.assertEqual(-33, i1.id)
+
+    def test_id6(self):
+        '''
+        Testing non-int value for id
+        '''
+        i1 = Base([1, 2, 3])
+        self.assertEqual([1, 2, 3], i1.id)
+
+    def test_base1(self):
+        '''
+        Testing int to JSON string function
+        '''
+        s1 = Square(33)
+        json_obj = s1.to_dictionary()
+        converted = Base.to_json_string([json_obj])
+        self.assertEqual(type(converted), str)
+
+    def test_base2(self):
+        '''
+        Testing multiple ints to JSON
+        '''
+        s1 = Square(33, 11, 9, 10)
+        json_obj = s1.to_dictionary()
+        converted = Base.to_json_string([json_obj])
+        self.assertEqual(converted, [{"id": 33, "size": 11, "x": 9, "y": 10}])
+
+    def test_base3(self):
+        '''
+        Testing sending None to to_json_string function
+        '''
+        s1 = Square(33, 11, 9, 10)
+        json_obj = s1.to_dictionary()
+        converted = Base.to_json_string(None)
+        self.assertEqual(converted, "[]")
+
+    def test_id7(self):
+        '''
+        Testing a dict for id
+        '''
+        i1 = Base({'id': 33})
+        self.assertEqual({'id': 33}, i1.id)
+
+    def test_id8(self):
+        '''
+        Testing tuple for id
+        '''
+        i1 = Base((33, 44))
+        self.assertEqual((33, 44), i1.id)
+
+    def test_base4(self):
+        '''
+        Testing an empty list to to_json_string function
+        '''
+        s1 = Square(33, 11, 9, 10)
+        json_obj = s1.to_dictionary()
+        converted = Base.to_json_string([])
+        self.assertEqual(converted, "[]")
--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-
+'''
+Creating tests for the Rectangle class and its methods
+'''
 import unittest
 from models.rectangle import Rectangle
 import json
@@ -9,126 +11,180 @@ from io import StringIO
 
 
 class test_rectangle(unittest.TestCase):
-
+    '''
+    Tests for Rectangle
+    '''
     def test_init(self):
-
+        '''
+        Creating an instance of Rectangle
+        '''
         self.r_instance = Rectangle(33, 11)
 
     def test_del(self):
-
+        '''
+        Deleting an instance of Rectangle
+        '''
         r_instance = Rectangle(1, 2)
         del(self.r_instance)
 
     def test_width_int(self):
-
+        '''
+        Testing the width variable getter
+        '''
         self.r_instance.width = 33
         self.assertEqual(33, self.r_instance.width)
 
     def test_height_int(self):
-
+        '''
+        Testing height variable getter
+        '''
         self.r_instance.height = 33
         self.assertEqual(33, self.r_instance.height)
 
     def test_x_int(self):
-
+        '''
+        Testing x variable getter
+        '''
         self.r_instance.x = 33
         self.assertEqual(33, self.r_instance.x)
 
     def test_y_int(self):
-
+        '''
+        Testing y variable getter
+        '''
         self.r_instance.y = 33
         self.assertEqual(33, self.r_instance.y)
 
     def test_rectangle_id(self):
-
+        '''
+        Testing the id of Rectangle
+        '''
         r_instance = Rectangle(33, 1, 2, 3, 4)
         self.assertEqual(4, r_instance.id)
 
     def test_width_non_int(self):
-
+        '''
+        Testing a non-int for width variable
+        '''
         with self.assertRaises(TypeError):
             r_instance = ("44", 33, 0)
 
     def test_height_non_int(self):
-
+        '''
+        Testing non-int for height variable
+        '''
         with self.assertRaises(TypeError):
             r_instance = (44, "33", 5, 6)
 
     def test_x_non_int(self):
-
+        '''
+        Testing non-int for x variable
+        '''
         with self.assertRaises(TypeError):
             r_instance = (44, 33, "5", 6)
 
     def test_y_non_int(self):
-
+        '''
+        Testing non-int for y variable
+        '''
         with self.assertRaises(TypeError):
             r_instance(44, 33, 5, "6")
 
     def test_width_negative(self):
-
+        '''
+        Testing a negative value for width variable
+        '''
         with self.assertRaises(TypeError):
             r_instance = (-1, 2, 3, 4)
 
     def test_height_negative(self):
-
+        '''
+        Testing a negative value for height variable
+        '''
         with self.assertRaises(TypeError):
             r_instance = (2, -3, 4, 5)
 
     def test_x_negative(self):
-
+        '''
+        Testing a negative value for x variable
+        '''
         with self.assertRaises(TypeError):
             r_instance = (2, 3, -4, 5)
 
     def test_y_negative(self):
-
+        '''
+        Testing a negative value for y variable
+        '''
         with self.assertRaises(TypeError):
             r_instance = (2, 3, 4, -5)
 
     def test_width_zero(self):
-
+        '''
+        Testing a zero value for width variable
+        '''
         with self.assertRaises(ValueError):
             r_instance = (0, 1)
 
     def test_height_zero(self):
+        '''
+        Testing a zero value for height variable
+        '''
         with self.assertRaises(ValueError):
             r_instance = (1, 0)
 
     def test_rectangle_area(self):
+        '''
+        Testing the area method in Rectangle
+        '''
         r_instance = Rectangle(3, 5)
         self.assertEqual(r_instance.area(), 3 * 5)
 
     def test_rectangle_str_overload(self):
-
+        '''
+        Testing the str overload method in Rectangle
+        '''
         r_instance = Rectangle(1, 2, 3, 4, 33)
         self.assertEqual(r_instance.__str__(), "[Rectangle] (33) 3/4 - 1/2")
 
     def test_rectangle_update_id(self):
-
+        '''
+        Testing the update method for id in Rectangle
+        '''
         self.r_instance.update(33)
         self.assertEqual(33, self.r_instance.id)
 
     def test_rectangle_update_width(self):
-
+        '''
+        Testing the update method for width in Rectangle
+        '''
         self.r_instance.update(33, 44)
         self.assertEqual(44, self.r_instance.width)
 
     def test_rectangle_update_height(self):
-
+        '''
+        Testing the update method for height in Rectangle
+        '''
         self.r_instance.update(33, 44, 55)
         self.assertEqual(55, self.r_instance.height)
 
     def test_rectangle_update_x(self):
-
+        '''
+        Testing the update method for x in Rectangle
+        '''
         self.r_instance.update(33, 44, 55, 66)
         self.assertEqual(66, self.r_instance.x)
 
     def test_rectangle_update_y(self):
-
+        '''
+        Testing the update method for y in Rectangle
+        '''
         self.r_instance.update(33, 44, 55, 66, 77)
         self.assertEqual(77, self.r_instance.y)
 
     def test_rectangle_update1(self):
-
+        '''
+        Testing the update method with **kwargs
+        '''
         self.r_instance.update(width=1, id=33, height=2, x=3, y=4)
         self.assertEqual(33, self.r_instance.id)
         self.assertEqual(1, self.r_instance.width)
@@ -137,7 +193,9 @@ class test_rectangle(unittest.TestCase):
         self.assertEqual(4, self.r_instance.y)
 
     def test_rectangle_update2(self):
-
+        '''
+        Testing update method with a mix of *args and **kwargs
+        '''
         self.r_instance.update(5, 7, 9, y=11, height=33)
         self.assertEqual(5, self.r_instance.id)
         self.assertEqual(7, self.r_instance.width)
@@ -146,3 +204,6 @@ class test_rectangle(unittest.TestCase):
         self.assertEqual(11, self.r_instance.y)
 
     def test_to_dictionary(self):
+        '''
+        Testing type returned in to_dictionary method
+        '''
