f0849b5d9d6499671ce819bf0f6ab68ec974528f
Liliana327 <894@holbertonschool.com>
2019-11-27 04:04:12 +0000

    script that lists all the cities of California that can be found in the database hbtn_0d_usa.

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,1 @@
+CREATE DATABASE if not exists force_name (id INT, name VARCHAR(256) not NULL);
---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,3 @@
+-- script that creates the table id_not_null
+CREATE TABlE if not exists id_not_null (id INT DEFAULT 1, name VARCHAR(256) not NULL);
+
