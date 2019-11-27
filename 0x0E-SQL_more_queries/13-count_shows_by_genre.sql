c6414e21e333efa4789f6e01926a28ad15bd3b2a
Liliana327 <894@holbertonschool.com>
2019-11-27 04:46:27 +0000

    add task number 13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,4 +1,4 @@
--- Import the database dump of hbtn_0d_tvshows
+-- Importe el volcado de la base de datos hbtn_0d_tvshows
 SELECT tv_genres.name as genre, COUNT(tv_show_genres.genre_id) as number_shows
-FROM tv_genres INNER JOIN tv_show_genres ON tv_show_genres.genre_id  = tv_genres.id GROUP BY tv_genres.name
+FROM tv_genres INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id GROUP BY tv_genres.name
 ORDER BY number_shows DESC;
