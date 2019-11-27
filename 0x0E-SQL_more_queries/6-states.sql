c1c710924e6eea28ce2f47960f3b530b69928d08
Liliana327 <894@holbertonschool.com>
2019-11-27 04:12:16 +0000

    modifaile task

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,4 @@
 -- creates the database hbtn_0d_usa and the table states
 CREATE DATABASE if not exists hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE if not exists states (id INT not NULL primary key auto_generated, name VARCHAR(256) not NULL);
+CREATE TABLE if not exists states (id INT not NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256) not NULL);
