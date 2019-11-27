02b25c0bba9a65d8fbb4570d38a2c2c5d3715e8c
Liliana327 <894@holbertonschool.com>
2019-11-27 04:08:19 +0000

    modifaile task

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,1 +1,2 @@
-CREATE TABLE if not exists force_name (id INT, name VARCHAR(256) not NULL);
+-- script that creates the table force_name
+CREATE TABLE if not exists force_name (id INT, name VARCHAR(256) NOT NULL);
