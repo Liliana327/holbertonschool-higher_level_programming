674d7899952abadd90807b4a2384047faa3e8c18
Liliana327 <894@holbertonschool.com>
2019-12-15 06:25:26 +0000

    Write a script that lists all states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''Get all states
+'''
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+    db_connection = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+        )
+
+    curs = db_connection.cursor()
+    curs.execute("SELECT * FROM states". format(username))
+    for row in curs.fetchall():
+        print(row)
+    curs.close()
+    db_connection.close()
+
