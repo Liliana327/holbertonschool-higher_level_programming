ecc6e283b2c2dede25ff1057385994251c46a70f
Liliana327 <894@holbertonschool.com>
2019-12-16 19:15:53 +0000

    All states via SQLAlchemy

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''script that lists all cities from the database
+'''
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+from model_state import State, Base
+
+if __name__ == '__main__':
+
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+    db_connection = create_engine("mysql+mysqldb://{:s}:{:s}@localhost/{:s}"
+                                  .format(username, password, database))
+
+    Base.metadata.create_all(db_connection)
+    session_basedata = sessionmaker(bind=db_connection)
+    init_basedata = session_basedata()
+    query = init_basedata.query(State)
+
+    for state in query.order_by(State.id).all():
+        print('{}: {}'.format(state.id, state.name))
+    init_basedata.close()
