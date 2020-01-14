98742eb8a41b1578f46d3a80d12b4708f7792055
Liliana327 <894@holbertonschool.com>
2020-01-14 06:26:39 +0000

    Star Wars API # 0

---
+++ 0x11-python-network_1/9-starwars.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''
+Star Wars API # 0
+'''
+if __name__ == "__main__":
+
+    import requests
+    from sys import argv
+
+    '''url del servicio http'''
+    url = 'https://swapi.co/api/people'
+
+    '''parametros de la consulta'''
+    params = {
+        'search': argv[1]
+    }
+
+
+    resp = requests.get(url, params=params).json()
+    print("Number of results: {}".format(resp.get('count')))
+    for i in resp.get('results'):
+        print(i.get('name'))
