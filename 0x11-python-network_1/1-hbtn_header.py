69756b427abaf96030e60a7d74895a7cb31f2784
Liliana327 <894@holbertonschool.com>
2020-01-14 02:44:36 +0000

    Response header value #0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,7 +5,5 @@ from urllib import request
 import sys
 
 '''url del servicio http'''
-url = 'https://intranet.hbtn.io/status'
-
 with request.urlopen(sys.argv[1]) as response:
         print(response.headers.get('X-Request-Id'))
