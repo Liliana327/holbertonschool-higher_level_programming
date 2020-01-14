62129593a17ca17343c9afd2806b228f3305e9bd
Liliana327 <894@holbertonschool.com>
2020-01-14 05:45:46 +0000

    API de bsqueda

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''Python script that takes in a letter and sends a POST request
+to http://0.0.0.0:5000/search_user with the letter as a parameter.
+'''
+if __name__ == "__main__":
+
+    import requests
+    from sys import argv
+    
+    url = 'http://0.0.0.0:5000/search_user'
+
+    params = {
+        'q': 'q'
+    }
+
+    q = argv[1] if len(argv) == 0 else ""
+    try:
+        resp = requests.post(url, params).json()
+        if 'id' in resp or 'name' in resp:
+            print("[{}] {}".format(resp['id'], resp['name']))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
