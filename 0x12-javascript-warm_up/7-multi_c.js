4065687b5943b472066fc3d1d6959547ab73b127
Liliana327 <894@holbertonschool.com>
2020-02-05 04:19:57 +0000

    print times C is fun

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const argument = process.argv;
+
+if (isNaN(argument[2])) {
+  console.log('Missing number of occurences');
+} else {
+
+let cont;
+
+  for (cont = 0; cont < argument[2]; cont++) {
+    console.log('C is fun');
+  }
+}
