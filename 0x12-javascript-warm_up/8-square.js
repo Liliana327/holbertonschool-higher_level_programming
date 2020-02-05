4002670d66decca083f61f7d0d0946870bc034a9
Liliana327 <894@holbertonschool.com>
2020-02-05 04:37:13 +0000

    print a square of x

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,9 +4,8 @@ const argument = process.argv;
 
 if (isNaN(argument[2])) {
   console.log('Missing size');
-
 } else {
-  let cont
+  let cont;
   for (cont = 0; cont < argument[2]; cont++) {
     console.log(Array(parseInt(argument[2]) + 1).join('X'));
   }
