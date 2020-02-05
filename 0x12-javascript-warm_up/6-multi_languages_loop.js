707a6c4a90dd28a7523573958d5c68827ff01615
Liliana327 <894@holbertonschool.com>
2020-02-05 04:11:57 +0000

    String matrix and a loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const MyList = ['C is fun', 'Python is cool', 'Javascript is amazing']
+const MyList = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
 let cont;
 
-for (cont = 0; cont < MyList.length; cont++){
+for (cont = 0; cont < MyList.length; cont++) {
   console.log(MyList[cont]);
 }
