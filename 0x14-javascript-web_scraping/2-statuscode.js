e8a19089e39ede3a1dbd878be1d3b0c8fb3bf300
Liliana327 <894@holbertonschool.com>
2020-02-12 00:50:40 +0000

    script that display the status code of a GET request.

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 
-// script that display the status code of a GET request. 
+// script that display the status code of a GET request.
 
 const request = require('request');
 
 request.get(process.argv[2], function (error, response) {
-
   if (error) {
     console.log(error);
   } else {
